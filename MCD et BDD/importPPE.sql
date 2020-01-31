2121c8a470bcccb490e89f8f8caa8b9a4a9eca79
Auros1805 <denise.hugo1805@gmail.com>
2020-01-31 11:11:55 +0100

    t

--- MCD et BDD/importPPE.sql
+++ MCD et BDD/importPPE.sql
@@ -1,12 +1,7 @@
-#CREATE DATABASE PPE
-#CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-#USE PPE;
-CREATE TABLE Utilisateur
-(
-idUtilisateur INT(11),
-idCompte INT(11),
-PRIMARY KEY (idUtilisateur)
-);
+CREATE DATABASE PPE
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE PPE;
+
 CREATE TABLE Compte
 (
 idCompte INT(11),
@@ -18,6 +13,7 @@ login VARCHAR(64),
 dateCreation DATE,
 nomCompte VARCHAR(64),
 idMessage INT(11),
+connexion TINYINT,
 PRIMARY KEY(idCompte)
 );
 CREATE TABLE FilDeDiscussion
@@ -25,7 +21,6 @@ CREATE TABLE FilDeDiscussion
 idFilDeDiscussion INT(11),
 idFilDeDiscussionClos INT(11),
 titreFilDeDiscussion VARCHAR(64),
-idUtilisateurNonConnecte INT(11),
 PRIMARY KEY(idFilDeDiscussion)
 );
 CREATE TABLE Message
@@ -37,28 +32,13 @@ titreMessage VARCHAR(64),
 idFilDeDiscussion INT(11),
 PRIMARY KEY(idMessage)
 );
-CREATE TABLE UtilisateurNonConnecte
-(
-idUtilisateurNonConnecte INT(11),
-PRIMARY KEY(idUtilisateurNonConnecte)
-);
-ALTER TABLE Utilisateur
-ADD CONSTRAINT Utilisateur_Compte
-FOREIGN KEY(idCompte)
-REFERENCES Compte(idCompte);
+
 ALTER TABLE Message
 ADD CONSTRAINT Message_idFilDeDiscussion
 FOREIGN KEY(idFilDeDiscussion)
 REFERENCES FilDeDiscussion(idFilDeDiscussion);
+
 ALTER TABLE Compte
 ADD CONSTRAINT Compte_idMessage
 FOREIGN KEY(idMessage)
 REFERENCES Message(idMessage);
-ALTER TABLE Utilisateur
-ADD CONSTRAINT Utilisateur_idUtilisateur
-FOREIGN KEY(idUtilisateur)
-REFERENCES UtilisateurNonConnecte(idUtilisateurNonConnecte);
-ALTER TABLE FilDeDiscussion
-ADD CONSTRAINT FilDeDiscussion_idUtilisateurNonConnecte
-FOREIGN KEY(idUtilisateurNonConnecte)
-REFERENCES UtilisateurNonConnecte(idUtilisateurNonConnecte);
