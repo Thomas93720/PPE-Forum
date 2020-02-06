80c49585b3ecc3fa73cb927e7d80ca45b9f301f2
Auros1805 <denise.hugo1805@gmail.com>
2020-02-06 09:56:46 +0100

    sardine

--- TestPart1/data/fildediscussion.php
+++ TestPart1/data/fildediscussion.php
@@ -1,4 +1,6 @@
 <?php
+    include("data/fildediscussion.php");
+	
     class FilDeDiscussion
     {
         private $idFilDeDiscussion;
@@ -6,7 +8,16 @@
         private $titreFilDeDiscussion;
         private $dateCreation;
         private $theme;
+        private $idCreateur;
         
+        function getIdCreateur() {
+            return $this->idCreateur;
+        }
+
+        function setIdCreateur($idCreateur) {
+            $this->idCreateur = $idCreateur;
+        }
+
         public function getIsFilDeDiscussionClos() {
             return $this->FilDeDiscussionClos;
         }
@@ -45,54 +56,7 @@
         public function setThemeFilDeDiscussion($theme) {
             $this->theme = $theme;
         }
-        public function getIdFilDeDiscussionWithId($idFilDeDiscussion)
-        {
-            $bdd=DatabaseLinker::getConnexion();
-            $state = $bdd->prepare("SELECT * FROM FilDeDiscussion WHERE idFilDeDiscussion=?");
-            $state->bindParam(1,$idFilDeDiscussion);
-            $state->execute();
-            $resultat = $state->fetchAll();
-            foreach ($resultat as $ligneResultat) 
-            {
-                $this->idFilDeDiscussion = $ligneResultat["idFilDeDiscussion"];
-                $this->dateCreation = $ligneResultat["dateOuverture"];
-                $this->titreFilDeDiscussion = $ligneResultat["titreFilDeDiscussion"];
-                $this->theme = $ligneResultat["Theme"];
-                $this->isFilDeDiscussionClos = $ligneResultat["isFilDeDiscussionClos"];
-            }
-        }
-        public static function getAllFilDeDiscussion($typeTri)
-        {
-            $tab= array();
-            $connex=DatabaseLinker::getConnexion();
-            $state=$connex->prepare("SELECT * FROM FilDeDiscussion ORDER BY ".$typeTri);
-            $state->execute();
-            $result = $state->fetchALL();
-            foreach ($result as $ligneResult) 
-            {
-                $user = new FilDeDiscussion();
-                $user->setIdFilDeDiscussion($ligneResult["idFilDeDiscussion"]);
-                $user->setDateCreation($ligneResult["dateOuverture"]);
-                $user->setTitreFilDeDiscussion($ligneResult["titreFilDeDiscussion"]);
-                $user->setThemeFilDeDiscussion($ligneResult["Theme"]);
-                $user->setIsFilDeDiscussionClos($ligneResult["isFilDeDiscussionClos"]);
-                $tab[] = $user;
-            }
-            return $tab;
-        }      
-        public static function getCreateurWithId($idCreateur)
-        {
-            $bdd=DatabaseLinker::getConnexion();
-            $state = $bdd->prepare("SELECT nomCompte FROM Compte INNER JOIN FilDeDiscussion ON FilDeDiscussion.idCreateur = Compte.idCompte WHERE idFilDeDiscussion=?");
-            $state->bindParam(1,$idCreateur);
-            $state->execute();
-            $resultat = $state->fetchAll();
-            $compte = new Compte();
-            foreach ($resultat as $ligneResultat) 
-            {
-                $compte->setNomCompte($ligneResultat["nomCompte"]);
-            }
-            return $compte;
-        }
+        
+        
     }
 ?>
