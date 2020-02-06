739ad50e3e1fd4d568872df00e482bea6d21704e
Neox63 <matchambon60@gmail.com>
2020-02-06 08:14:43 +0100

    rr

---
+++ ForumPPE/indexRegister.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="Style.css">
+    <link rel="stylesheet" href="https://kit.fontawesome.com/92920db574.js" />
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<script src="https://kit.fontawesome.com/92920db574.js" crossorigin="anonymous"></script>
+    <title></title>
+</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 offset=md-1">
+					<div class="row">
+						<div class="col-md-5 register-left">
+							<img src="image/logoJB.jpg">
+							<h3>S'inscrire</h3>
+							<p>Inscrivez vous dès maintenant sur le forum du BTS SIO!</p>
+							<button type="button" class="btn btn-primary">À propos</button>
+						</div>
+						<div class="col-md-7 register-right">
+							<h2>S'inscrire ici</h2>
+							<div class="register-form">
+								<div class="form-group">
+									<input type="text" name="form-control" placeholder="Pseudo">
+								</div>
+
+								<div class="form-group">
+									<input type="email" name="form-control" placeholder="Email">
+								</div>
+
+								<div class="form-group">
+									<input type="password" name="form-control" placeholder="Mot de passe">
+								</div>
+								<button type="button" class="btn btn-primary">S'inscrire</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
