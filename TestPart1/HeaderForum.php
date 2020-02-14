e688a2d3ea55016b587231a301329d92e8bc7cdf
Auros1805 <denise.hugo1805@gmail.com>
2020-02-14 10:10:23 +0100

    jpp

--- TestPart1/HeaderForum.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <link rel="Stylesheet" type="text/css" href="StyleForum.css" media="all"/>
-    <title>Forum</title>
-</head>
-<body>
