d7dd5f492c30da98a15d029fc1e62bd6cc621d23
Auros1805 <denise.hugo1805@gmail.com>
2020-02-19 08:29:13 +0100

    dfb

---
+++ TestPart1/data/message.php
@@ -0,0 +1,103 @@
+<?php
+    class Message
+    {
+        private $idMessage;
+        private $libelle;
+        private $dateEnvoi;
+        private $titreMessage;
+        private $idFilDeDiscussion;
+        
+        function getIdMessage() {
+            return $this->idMessage;
+        }
+
+        function getLibelle() {
+            return $this->libelle;
+        }
+
+        function getDateEnvoi() {
+            return $this->dateEnvoi;
+        }
+
+        function getTitreMessage() {
+            return $this->titreMessage;
+        }
+
+        function getIdFilDeDiscussion() {
+            return $this->idFilDeDiscussion;
+        }
+
+        function setIdMessage($idMessage) {
+            $this->idMessage = $idMessage;
+        }
+
+        function setLibelle($libelle) {
+            $this->libelle = $libelle;
+        }
+
+        function setDateEnvoi($dateEnvoi) {
+            $this->dateEnvoi = $dateEnvoi;
+        }
+
+        function setTitreMessage($titreMessage) {
+            $this->titreMessage = $titreMessage;
+        }
+
+        function setIdFilDeDiscussion($idFilDeDiscussion) {
+            $this->idFilDeDiscussion = $idFilDeDiscussion;
+        }
+        public function getMessageWithId($idMessage)
+        {
+            $bdd=DatabaseLinker::getConnexion();
+            $state = $bdd->prepare("SELECT * FROM Message WHERE idMessage=?");
+            $state->bindParam(1,$idMessage);
+            $state->execute();
+            $resultat = $state->fetchAll();
+            foreach ($resultat as $ligneResultat) 
+            {
+                $this->idMessage = $ligneResultat["idMessage"];
+                $this->dateEnvoi = $ligneResultat["dateEnvoi"];
+                $this->libelle = $ligneResultat["libelle"];
+                $this->titreMessage = $ligneResultat["titreMessage"];
+                $this->idFilDeDiscussion = $ligneResultat["idFilDeDiscussion"];
+            }
+        }
+        public static function getAllMessage($typeTri)
+        {
+            $tab= array();
+            $connex=DatabaseLinker::getConnexion();
+            $state=$connex->prepare("SELECT * FROM Message ORDER BY ".$typeTri);
+            $state->execute();
+            $result = $state->fetchALL();
+            foreach ($result as $ligneResult) 
+            {
+                $msg = new Message();
+                $msg->setIdMessage($ligneResult["idMessage"]);
+                $msg->setDateEnvoi($ligneResult["dateEnvoi"]);
+                $msg->setLibelle($ligneResult["libelle"]);
+                $msg->setTitreMessage($ligneResult["titreMessage"]);
+                $msg->setIdFilDeDiscussion($ligneResult["idFilDeDiscussion"]);
+                $tab[] = $msg;
+            }
+            return $tab;
+        }
+        public function findMessageWithIdFilDeDiscussion($idFilDeDiscussion)
+        {
+            $tab = array();
+            $connex=DatabaseLinker::getConnexion();
+            $state=$connex->prepare("SELECT * FROM Message WHERE idFilDeDiscussion =? ORDER BY dateEnvoi");
+            $state->BindParam(1,$idFilDeDiscussions);
+            $state->execute();
+            $result = $state->fetchALL();
+            foreach ($result as $ligneResultat) 
+            {
+                $this->idMessage = $ligneResultat["idMessage"];
+                $this->dateEnvoi = $ligneResultat["dateEnvoi"];
+                $this->libelle = $ligneResultat["libelle"];
+                $this->titreMessage = $ligneResultat["titreMessage"];
+                $this->idFilDeDiscussion = $ligneResultat["idFilDeDiscussion"];
+            }
+        }
+
+    }
+?>
---
+++ TestPart1/index.php
@@ -0,0 +1,100 @@
+<?php
+	include("header.php");
+	include_once("datamanagers/DatabaseLinker.php");
+	include("datamanagers/fildediscussionManager.php");
+	include("datamanagers/compteManager.php");
+	include("datamanagers/messageManager.php");
+	if(isset($_SESSION["idUser"]))  
+	{
+		session_destroy();
+	}
+?>
+<?php
+	include("BarreDeNavNonCo.php");
+?>
+<div class="page">
+	<div class="partgauche">
+		<img class = "pub" src="https://data.whicdn.com/images/177181124/original.jpg">
+		<img class="pub" src="https://pbs.twimg.com/media/EAjs3ZEXoAEON26.jpg">
+	</div>
+	<div class="milieu">
+        <?php
+        	$typeTriFilDeDiscussion = "dateOuverture ASC";
+			$tabFilDeDiscussion = fildediscussionManager::getAllFilDeDiscussion($typeTriFilDeDiscussion);
+			$taille = sizeof($tabFilDeDiscussion)/6;
+			$reste = fmod(sizeof($tabFilDeDiscussion),6);
+			if (empty($_GET["pages"])|| $_GET["pages"]=="1")
+			{
+				for ($i=1; $i < 7; $i++) 
+                                { 
+					$fildediscussion = new FilDeDiscussion();
+					$fildediscussion = fildediscussionManager::getFilDeDiscussionWithId($i);
+					$createur = fildediscussionManager::getCreateurWithId($i);
+                                        echo '<a class="lien" href="Forum.php?index='.$fildediscussion->getIdFilDeDiscussion().'">';
+						echo '<div class="box">';
+							echo '<div class="Content">';
+								echo '<img class="imageTheme" src="image/Theme/'.$fildediscussion->getThemeFilDeDiscussion().'.png">';
+								echo '<div>';
+									echo'<div class="grand">'.$fildediscussion->getTitreFilDeDiscussion().'</div>';
+									echo '<div class="petit">'.'Createur : '.$createur->getNomCompte().'<br>Theme : '.$fildediscussion->getThemeFilDeDiscussion().'<br>date ouverture : '.$fildediscussion->getDateCreation().'</div>';
+								echo '</div>
+							</div>
+						</div>
+					</a>';
+				}
+			}
+			$cpt=7;
+			if (!empty($_GET["pages"])) 
+			{
+				if ($reste>0) 
+				{
+					$taille = $taille+1;
+				}
+				for ($i=2; $i < $taille+1; $i++) 
+				{ 
+					if ($_GET["pages"]==$i) 
+					{
+						for ($i=$cpt; $i < $cpt+6; $i++) 
+			            { 
+							$fildediscussion = new FilDeDiscussion();
+							$fildediscussion = fildediscussionManager::getFilDeDiscussionWithId($i);
+							$createur = fildediscussionManager::getCreateurWithId($i);
+					        echo '<a class="lien" href="Forum.php?index='.$fildediscussion->getIdFilDeDiscussion().'">';
+								echo '<div class="box">';
+									echo '<div class="Content">';
+										echo '<img class="imageTheme" src="image/Theme/'.$fildediscussion->getThemeFilDeDiscussion().'.png">';
+										echo '<div>';
+											echo'<div class="grand">'.$fildediscussion->getTitreFilDeDiscussion().'</div>';
+											echo '<div class="petit">'.'Createur : '.$createur->getNomCompte().'<br>Theme : '.$fildediscussion->getThemeFilDeDiscussion().'<br>date ouverture : '.$fildediscussion->getDateCreation().'</div>';
+										echo '</div>
+									</div>
+								</div>
+							</a>';
+						}
+						$cpt=$cpt+7;
+					}
+				}
+			}
+            
+			$nbpages = $taille;
+				
+			?>
+		
+	</div>
+	<div class="partdroite">
+		<img class="pub" src="https://media.discordapp.net/attachments/535910358078980172/676726300110618645/DS8NwTEX0AAs8Kl.jpg?width=674&height=943">
+		<img class="pub" src="https://kimhandysidesvoiceover.com/wp-content/uploads/2017/08/Colget-Funny-Commercial-Picture.jpg">
+	</div>
+</div>
+
+<div class="bas">
+	<?php  
+  	for ($i=1; $i < $nbpages+1; $i++) 
+  	{ 
+  		echo '<a href="index.php?pages='.$i.'" class="nbPages">'.$i.'</a>';
+  	}
+	?>
+</div>
+<?php
+	include("footer.php");
+?>
