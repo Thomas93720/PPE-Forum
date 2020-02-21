910d5d02e16cdc5b4359e190487d5f46df076102
Unknown <unknown@example.com>
2020-02-21 09:25:20 +0100

    test

--- TestPart1/footer.php
+++ TestPart1/footer.php
@@ -1,2 +1,24 @@
-﻿</body>
+    <footer>
+      <div class="footer-container">
+        <div class="left-col">
+          <div class="social-media">
+            <a href="http://sio.jbdelasalle.com"><i class="fas fa-school"></i></a>
+            <a href="indexRulePage"><i class="fas fa-scroll"></i></a>
+            <a href="#returnTop"><i class="fas fa-angle-double-up"></i></a>
+          </div>
+          <p class="rights-text">© 2020 Créé par Thomas PREZOT, Mathis CHAMBON et Hugo DENISE</p>
+        </div>
+
+        <div class="right-col">
+          <h1>S'inscrire à notre NewsLetter</h1>
+          <div class="border"></div>
+          <p>Entrez votre email pour recevoir des informations sur le Forum</p>
+          <form action="" class="newsletter-form">
+            <input type="text" class="txtb" placeholder="Entrez votre email">
+            <input type="submit" class="btn" value="Envoyer">
+          </form>
+        </div>
+      </div>
+    </footer>
+  </body>
 </html>
