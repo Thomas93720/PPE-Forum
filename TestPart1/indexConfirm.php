c81aabce0f21d76d0a8989b0643115955626fd5f
Neox63 <matchambon60@gmail.com>
2020-02-21 11:04:49 +0100

    e

---
+++ TestPart1/indexConfirm.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bienvenue sur le Forum!</title>
+	<link rel="stylesheet" type="text/css" href="styleTeamPage.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://kit.fontawesome.com/92920db574.js" />
+	<script src="https://kit.fontawesome.com/92920db574.js" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="topSection">
+			<h1>Bienvenue sur le Forum!</h1>
+			<p>Votre inscription a bien été prise en compte!</p>
+		</div>
+		<div class="bottomSection">
+			<a href="indexRulePage.php"><button>Règles du Forum</button></a>
+			<a href="index.php"><button>Accéder au Forum</button></a>
+			<a href="indexInfo.php"><button>Équipe du PPE</button></a>
+		</div>
+	</div>
+</body>
+</html>
