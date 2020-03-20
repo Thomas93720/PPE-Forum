1e5673e5cebd8ad418b27d4970b9e41e8c48998d
Neox63 <matchambon60@gmail.com>
2020-03-20 11:47:59 +0100

    e

--- TestPart1/AccountPage.php
+++ TestPart1/AccountPage.php
@@ -2,67 +2,38 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <link rel="Stylesheet" type="text/css"href="Style/AccountPage.css" href="AccountPage.css" media="all"/>
+    <link rel="Stylesheet" type="text/css" href="AccountPage.css" media="all"/>
     <link href="https://fonts.googleapis.com/css?family=Bitter&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://kit.fontawesome.com/92920db574.js" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 	<script src="https://kit.fontawesome.com/92920db574.js" crossorigin="anonymous"></script>
-    <title>Forum</title>
+    <title>Modification de vos informations</title>
 </head>
 <body>
+	<div class="container">
+		<h1>Modifier mes informations</h1>
 
-<h1>Modifier mes informations</h1>
-<a href="#">Changer mon image</a>
-<h2>Changer de mot de passe</h2>
-<form method="POST">
-	<br>
-	<label>Votre ancien mot de passe : </label>
-	<input type="password" name="mdp">
-	<?php
-	if (!empty($_POST["mdp"])) 
-	{
-		?>
-		<br><br>
-		<label>Votre nouveau mot de passe : </label>
-		<input type="password" name="newmdp">
-		<?php
-	}
-	?>
-	<input type="submit" name="valider">
-</form>
-<h2>Changer votre email</h2>
-<form method="POST">
-	<label>Votre ancienne email : </label>
-	<input type="email" name="email">
-	<?php
-	if (!empty($_POST["email"])) 
-	{
-		?>
-		<br><br>
-		<label>Votre nouvelle email : </label>
-		<input type="mail" name="newMail">
-		<?php
-	}
-	?>
-	<input type="submit" name="valider">
-</form>
-<h2>Changer votre pseudo</h2>
-<form method="POST">
-	<label>Votre ancien pseudo</label>
-	<input type="text" name="pseudo">
-	<?php
-	if (!empty($_POST["pseudo"])) 
-	{
-		?>
-		<label>Votre nouveau pseudo : </label>
-		<input type="text" name="newPseudo">
-		<?php
-	}
-	?>
-	<input type="submit" name="valider">
-</form method="POST">
-<br>
-<h2>Biographie</h2>
-<form>
-	<textarea></textarea>
-</form>
+		<a href="Avatar.php">Changer votre avatar</a>
+
+		<h2>Changer de mot de passe</h2>
+		<form method="POST">
+			<p>Entrez votre ancien mot de passe : </p>
+			<input class="pass" type="password" name="mdp">
+			<p>Entrez votre nouveau mot de passe :</p>
+			<input class="pass" type="password" name="mdp">
+			<p>Confirmez votre nouveau mot de passe :</p>
+			<input class="pass" type="password" name="mdp">
+			<br>
+			<input type="submit" name="valider">
+		</form>
+
+		</form method="POST">
+		<br>
+		<h2 class="bio">Changez votre biographie : </h2>
+		<form>
+			<textarea placeholder="Décrivez vous..."></textarea>
+			<br>
+			<input type="submit" name="valider">
+		</form>
+	</div>
+
